9c82d50fa08281f58b69dc4e8577d488cf788dfd
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-08-31 20:13:35 +0800

    JSX behind the scenes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const firstName = "John";
-  const lastName = "Wick";
-  const age = 28;
-  const job = "Web Developer";
-  const inputPlaceholder = "Enter your details";
-  const mArr = [0, 1, 2, 3, 4]
-
-  const getFullName = (firstName, lastName) => {
-    return `${firstName} ${lastName}`
+  const blogObj = {
+    title: 'Blog Title 1',
+    description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
   }
-
-  const detailsInputBox = <input placeholder={inputPlaceholder}></input>;
 
   return (
     <div className="App">
-      <h3>Full Name: {getFullName(firstName, lastName)}</h3>
-      <p>Age: {age}</p>
-      <p>Job: {job}</p>
-
-      <input placeholder={inputPlaceholder}></input>
-
-      {detailsInputBox}
-
-      {mArr[0]} 
+      <div>
+        <h3>{blogObj.title}</h3>
+        <p>{blogObj.description}
+        </p>
+      </div>
+      <hr></hr>
+      <div>
+        <h3>{blogObj.title}</h3>
+        <p>{blogObj.description}
+        </p>
+      </div>
+      <hr></hr>
+      <div>
+        <h3>{blogObj.title}</h3>
+        <p>{blogObj.description}
+        </p>
+      </div>
     </div>
   );
 }
