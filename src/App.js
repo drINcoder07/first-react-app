282691d2cfa04f52e7e67c4e6f37fd636d985ae0
Don Aldrin Beringuela <drinberinguela@gmail.com>
2022-08-31 20:33:51 +0800

    Create React elements dynamically

--- src/App.js
+++ src/App.js
@@ -7,27 +7,38 @@ function App() {
     description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
   }
 
+  const blogArr = [
+    {
+      title: 'Blog Title 1',
+      description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+    },
+    {
+      title: 'Blog Title 1',
+      description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+    },
+    {
+      title: 'Blog Title 1',
+      description: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+    }
+  ]
+
+  const blogCards = blogArr.map((item, pos) => {
+    console.log(item);
+
+    return (
+      <div className='blogCard' key={pos}>
+      <h3>{item.title}</h3>
+      <p>{item.description}
+      </p>
+      </div>
+    )
+  })
+
   // const styles = 
 
   return (
     <div className="App">
-      <div className='blogCard'>
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}
-        </p>
-      </div>
-      <hr></hr>
-      <div className='blogCard'>
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}
-        </p>
-      </div>
-      <hr></hr>
-      <div className='blogCard'>
-        <h3>{blogObj.title}</h3>
-        <p>{blogObj.description}
-        </p>
-      </div>
+      {blogCards}
     </div>
   );
 }
